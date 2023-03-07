177860fa3cd1946e206927f111fe5ab74f309d90
hashamzaki <hashamzaki123@gmail.com>
2023-03-07 19:58:12 +0500

    line spacing fixed

--- parser.py
+++ parser.py
@@ -3,22 +3,28 @@ from datastructures import WeatherData
 
 
 class Parser:
-    @staticmethod
-    def parser(files_data):
+
+    def parser(self,files_data):
         calculation_instance = Calculator()
         for file_data in files_data:
-            file_name = [file_data[1]]
-            for line in file_data[2:]:
-                values = line.replace('\n', '').split(',')[1:]
-                day_data = [line.split(',')[0]]
-                for value in values:
-                    if value.replace('-', '').isdigit():
-                        day_data.append(int(value))
-                    elif value.replace('.', '', 1).replace('-', '').isdigit():
-                        day_data.append(float(value))
-                    else:
-                        day_data.append(value)
-
-                calculation_instance.weather_data.append(WeatherData(file_name, day_data))
+            self.pars_file_data(file_data)
 
         return calculation_instance
+
+    def pars_file_data(self, file_data):
+        file_name = [file_data[1]]
+        for line in file_data[2:]:
+            values = line.replace('\n', '').split(',')[1:]
+            day_data = [line.split(',')[0]]
+            for value in values:
+                self.type_cast_value(value)
+
+            self.calculation_instance.weather_data.append(WeatherData(file_name, day_data))
+
+    def type_cast_value(self,value):
+        if value.replace('-', '').isdigit():
+            self.day_data.append(int(value))
+        elif value.replace('.', '', 1).replace('-', '').isdigit():
+            self.day_data.append(float(value))
+        else:
+            self.day_data.append(value)
