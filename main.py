9ea110a0f1ce52978ab31a16d85f41184716edac
hashamzaki <hashamzaki123@gmail.com>
2023-03-11 17:19:43 +0500

    main.py cleaning

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-from inputvalidations import Validator
 from controller import Controller
-from utils import read_file_path, read_year, read_switch, is_digit, typcast_year
-
+from utils import read_sys_argument_file_path, read_sys_argument_year, type_cast_year
+from validators import InputValidator
 if __name__ == '__main__':
-    if (
-            Validator.is_argument_valid() and read_switch() == '-e' and Validator.is_dir()
-            and Validator.is_year_valid()
-    ):
-        year = typcast_year(read_year())
-        file_path = read_file_path()
-        Controller.print_yearly_data(file_path, year)
-    elif (
-
-            Validator.is_argument_valid() and read_switch() == '-a'
-            and Validator.is_dir() and Validator.is_year_valid()
-    ):
-        year = typcast_year(read_year())
-        file_path = read_file_path()
-        Controller.print_yearly_data(file_path, year)
+    if InputValidator.yearly_data_calculation_conditions():
+        Controller.print_yearly_data(read_sys_argument_file_path(), type_cast_year(read_sys_argument_year()))
+    elif InputValidator.all_year_data_calculation_conditions():
+        Controller.print_yearly_data(read_sys_argument_file_path(), type_cast_year(read_sys_argument_year()))
     else:
         raise Exception('invalid argument')
-
--- utils.py
+++ utils.py
@@ -24,27 +24,27 @@ def is_float(value):
     return value.replace('.', '', 1).replace('-', '').isdigit()
 
 
-def comparison_operation(operation_description, data_to_be_compared, comaring_value):
+def comparison_operation(operation_description, data_to_be_compared, comparing_value):
     if operation_description == MapperIndex.MAX_OPERATION_STRING:
-        return_value = data_to_be_compared > comaring_value
+        return_value = data_to_be_compared > comparing_value
     elif operation_description == MapperIndex.MIN_OPERATION_STRING:
-        return_value = data_to_be_compared < comaring_value
+        return_value = data_to_be_compared < comparing_value
     else:
         raise Exception('invalid argument')
 
     return return_value
 
 
-def read_file_path():
-    return sys.argv[1]
+def read_sys_argument_file_path():
+    return sys.argv[MapperIndex.ARGUMENT_FILE_PATH_INDEX]
 
 
-def read_switch():
-    return sys.argv[2]
+def read_sys_argument_switch():
+    return sys.argv[MapperIndex.ARGUMENT_SWITCH_INDEX]
 
 
-def read_year():
-    return sys.argv[3]
+def read_sys_argument_year():
+    return sys.argv[MapperIndex.ARGUMENT_YEAR_INDEX]
 
 
 def type_cast_year(year):
